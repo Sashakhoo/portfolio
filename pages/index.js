9aab9a15a5b19a79e4a7b3cae6998892dd023a46
sashakhoo <sashakhoo8@gmail.com>
2023-06-07 18:19:08 +0800

    description updates

--- pages/index.js
+++ pages/index.js
@@ -57,8 +57,7 @@ export default function Home() {
               Software Engineer | Machine Learning Enthusiasts
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+            Relentless dream chaser fueled by ambition and caffeine
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <a href="https://www.linkedin.com/in/natasyakhoo/" target="_blank"><AiFillLinkedin /></a>
