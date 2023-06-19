201c6e596f0b2530b3574b2be8a01f880fc43f91
sashakhoo <sashakhoo8@gmail.com>
2023-06-19 11:14:29 +0800

    updated skills

--- pages/SkillCard.js
+++ pages/SkillCard.js
@@ -5,7 +5,7 @@ export default function SkillCard(props){
         <div data-aos="fade-up" data-aos-duration="500" data-aos-offset="100" className="w-11/12  p-4 text-center justify-center border-2 border-gray-800 bg-dark-200 rounded-md flex flex-col h-48">
             <Image src={props.img} className="w-20 max-h-20 mx-auto" alt={props.name}></Image>
             <div className="mt-2">
-                <h1 className="font-bold md:text-xl">{props.name}</h1>
+                <h1 className="font-bold md:text-xl dark:text-white">{props.name}</h1>
             </div>
         </div>
     )
