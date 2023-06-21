de3779b50e6e1445d1720b897fa2e4c0a62f8cd6
sashakhoo <sashakhoo8@gmail.com>
2023-06-21 16:03:33 +0800

    modified cert

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ import code from "../public/main/code.png";
 import design from "../public/main/design.png";
 import consulting from "../public/main/consulting.png";
 import Image from "next/image";
-import web1 from "../public/project/web1.png";
+
+import mern from "../public/cert/mern.png";
 import web2 from "../public/project/web2.png";
 import web3 from "../public/project/web3.png";
 import web4 from "../public/project/web4.png";
@@ -177,11 +178,7 @@ export default function Home() {
         <section className="py-10">
           <div>
           <hr></hr>
-            <h3 className="text-2xl font-bold flex justify-center py-10 dark:text-white ">CERTIFICATION</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
+            <h3 className="text-2xl font-bold flex justify-center py-10 dark:text-white ">CERTIFICATES</h3>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
@@ -190,7 +187,7 @@ export default function Home() {
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web1}
+                src={mern}
               />
             </div>
             <div className="basis-1/3 flex-1">
